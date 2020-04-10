d88e4bf8318e2ddccfdc486c3067f90a1d0a869c
Rigo <jfkbeatkid@Rigos-MacBook-Pro.local>
2020-04-10 12:11:03 -0700

    cleaned up presentation logic

--- src/components/PlantMap.js
+++ src/components/PlantMap.js
@@ -7,6 +7,22 @@ const attribution = '<a href="http://osm.org/copyright">OpenStreetMap</a> contri
 export default () => {
   const [markers, handleClick, handleDelete, handleInputChange] = useMapLogic()
 
+  const renderMarker = data => {
+    return (
+      <Marker key={data.key} position={data.position} draggable="true">
+        <Popup onClick={handleDelete}>
+          <form className="ui form">
+            <div className="field">
+              <label>Plant Name</label>
+              <input value={data.content} onChange={(e) => handleInputChange(e.target.value, data)}/>
+            </div>
+            <button className="ui small button red" onClick={() => handleDelete(data.position)}>Delete</button>
+          </form>
+        </Popup>
+      </Marker>
+    )
+  }
+
   return (
     <Map
       center={[47.6062, -122.3321]}
@@ -18,14 +34,7 @@ export default () => {
         attribution={attribution}
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-    {markers.map(data =>
-        <Marker key={data.key} position={data.position} draggable="true">
-        <Popup onClick={handleDelete}>
-          <input value={data.content} onChange={(e) => handleInputChange(e.target.value, data)}/>
-          <button onClick={() => handleDelete(data.position)}>X</button>
-        </Popup>
-      </Marker>
-      )}
+      {markers.map(data => renderMarker(data))}
     </Map>
   )
 
